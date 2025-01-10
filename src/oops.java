e15b18ab4a4b5d4fe0655b1cc6957896be3599ce
Hordhik <2300031095@kluniversity.in>
2025-01-10 12:26:18 +0530

    Added examples of constructor

--- src/oops.java
+++ src/oops.java
@@ -16,10 +16,37 @@ class ClassName {
     }
 }
 
+class MyClass {
+    int x;
+    String y;
+
+    // Default constructor
+    MyClass() {
+        x = 5;
+    }
+    // Parameterized constructor
+    MyClass(int val) {
+        x = val;
+    }
+    //Constructor Overloading
+    MyClass(int val, String str) {
+        x = val;
+        y = str;
+    }
+}
+
 
 public class oops {
     public static void main(String[] args) {
         ClassName obj = new ClassName(10, "Java"); // obj instance(object) of ClassName
         obj.display();  // Output: 10 Java
+
+        System.out.println("Constructor: ");
+        MyClass constructor1 = new MyClass();
+        MyClass constructor2 = new MyClass(10);
+        MyClass constructor3 = new MyClass(7, "Java");
+        System.out.println("\t"+constructor1.x + "\n\t" + constructor2.x + "\n\t" + constructor3.x + " " + constructor3.y);
+
+
     }
 }
