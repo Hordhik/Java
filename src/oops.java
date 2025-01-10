5fe1dbf394b993140b891ba4998599639e7bbae4
Hordhik <2300031095@kluniversity.in>
2025-01-10 12:34:28 +0530

    Added examples of Inheritance

--- src/oops.java
+++ src/oops.java
@@ -35,17 +35,37 @@ class MyClass {
     }
 }
 
+class Animal {
+    Animal() {
+        System.out.println("Animal constructor");
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        super();  // Calls the constructor of Animal
+        System.out.println("Dog constructor");
+    }
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
 
 public class oops {
     public static void main(String[] args) {
         ClassName obj = new ClassName(10, "Java"); // obj instance(object) of ClassName
         obj.display();  // Output: 10 Java
 
-        System.out.println("Constructor: ");
+        System.out.println("\nConstructor: ");
         MyClass constructor1 = new MyClass();
         MyClass constructor2 = new MyClass(10);
         MyClass constructor3 = new MyClass(7, "Java");
-        System.out.println("\t"+constructor1.x + "\n\t" + constructor2.x + "\n\t" + constructor3.x + " " + constructor3.y);
+        System.out.println(constructor1.x + "\n" + constructor2.x + "\n" + constructor3.x + " " + constructor3.y);
+
+        System.out.println("\nInheritance: ");
+        Dog d = new Dog();//super
+        d.sound();//method override
 
 
     }
