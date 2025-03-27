b94f72bdaf072663373449d398e69af869ead61a
Konda Hordhik Manikant Reddy <hordhikmanikant@Hordhiks-MacBook-Air.local>
2025-03-27 20:12:46 +0530

    Example of VarArgs

---
+++ src/VarArgs.java
@@ -0,0 +1,15 @@
+
+
+public class VarArgs {
+    public static void main(String[] args) {
+        printNumbers(1, 2, 3, 4, 5);  // Output: 1 2 3 4 5
+        printNumbers(10, 20);         // Output: 10 20
+        printNumbers();               // Output: (Nothing, empty array)
+    }
+    public static void printNumbers(int... numbers) {
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
